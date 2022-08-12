9edddc4c4086e7bb6bb0d4ea15c3531bdd590ab3
muradzeynal <muradzeynalov085@gmail.com>
2022-08-12 14:22:18 +0400

    first

---
+++ ReqemTexmin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReqemTexmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int texmin = 0, saygac = 0;
+            Random myNum = new Random();
+            int reqem = myNum.Next(0, 100);
+            Console.WriteLine("******Number Game******");
+            Console.WriteLine("0-100 arasi reqem texmin edin!");
+            while (texmin != reqem)
+            {
+                saygac++;
+                texmin = Convert.ToInt32(Console.ReadLine());
+
+                if (texmin > reqem)
+                {
+                    Console.WriteLine($"{texmin} den kicik reqem daxil edin");
+                }
+                else if (texmin < reqem)
+                {
+                    Console.WriteLine($"{texmin} den boyuk reqem daxil edin");
+                }
+
+
+            }
+            Console.WriteLine($"Tebrikler siz {saygac} defeye duzgun tapdiniz!");
+            Console.Read();
+        }
+       
+    }
+}
